1c9b056e2781e15fc1d1c1f513a59809342cc40e
Yuli Wang <yuliwang1007@gmail.com>
2024-03-23 16:13:23 -0600

    create CustomButton component and CustomButtonProps interface

---
+++ components/CustomButton.tsx
@@ -0,0 +1,26 @@
+"use client";
+// need to convert the CustomButton into a client side component, on default, all components are server side component
+import React from "react";
+import Image from "next/image";
+import { CustomButtonProps } from "@/types";
+
+//if a component has props, need to delcear the props type first!!!
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) => {
+  //need to create types folder and inside types.index.ts, declear and export the interface CustomButtonProps
+  return (
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  );
+};
+
+export default CustomButton;
--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,5 @@
 //Create this file to export all components so that app/page.tsx can import all of the components
-import { Hero } from "./Hero";
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
 
-export {
-  Hero
-}
-
+export { Hero, CustomButton };
