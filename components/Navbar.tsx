900589105f2e84eb3f3f4a1b7a087e3f9098afee
Yuli Wang <yuliwang1007@gmail.com>
2024-03-25 18:43:09 -0600

    fix hero image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ import { CustomButton } from '.'
 const Navbar = () => {
   return (
    <header className='w-full absolute z-10'>
-     <nav className='max-w-[1440px] mx-autp flex justify-between items-center sm:px-16 px-6 py-4'>
+     <nav className='mx-autp flex justify-between items-center sm:px-16 px-6 py-4'>
        <Link href="/" className='flex justify-center items-center'>
         <Image 
           src="/logo.svg"
