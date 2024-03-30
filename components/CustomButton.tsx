e6981d0538da7dc809af9111986e0266552e4fec
Yuli Wang <yuliwang1007@gmail.com>
2024-03-30 12:46:56 -0600

    carCard add rightIcon to CustomButton

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -22,7 +22,17 @@ const CustomButton = ({
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image 
+           src={rightIcon}
+           alt="right icon"
+           fill
+           className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 };
