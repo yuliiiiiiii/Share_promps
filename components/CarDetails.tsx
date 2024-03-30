d65eb9ecd277cec626e30ef4b36e84909fc082b4
Yuli Wang <yuliwang1007@gmail.com>
2024-03-30 14:14:42 -0600

    CarDetail add close Modal button

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,5 +1,9 @@
+"use client"
 import { CarProps } from '@/types';
 import React from 'react'
+import Image from 'next/image';
+import { Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
 
 
 interface CarDetailsProps {
@@ -10,7 +14,55 @@ interface CarDetailsProps {
 
 const CarDetails = ({isOpen, closeModal, car} :CarDetailsProps) => {
   return (
-    <div>CarDetails</div>
+    <>
+      <Transition appear 
+       show={isOpen} as={Fragment}>
+       <Dialog as='div' className="relative z-10" onClose={closeModal}>
+        <Transition.Child
+         as={Fragment}
+         enter='ease-out duration-300'
+         enterFrom='opacity-0'
+         enterTo='opacity-100'
+         leave='ease-in duration-200'
+         leaveFrom='opacity-100'
+         leaveTo='opacity-0'
+        >
+         <div className='fixed inset-0 bg-black bg-opacity-25' />
+        </Transition.Child>
+
+        <div className='fixed inset-0 overflow-y-auto'>
+          <div className='flex min-h-full items-center justify-center p-4 text-center'>
+            <Transition.Child
+              as={Fragment}
+              enter='ease-out duration-300'
+              enterFrom='opacity-0 scale-95'
+              enterTo='opacity-100 scale-100'
+              leave='ease-in duration-200'
+              leaveFrom='opacity-100 scale-100'
+              leaveTo='opacity-0 scale-95'
+            >
+              <Dialog.Panel>
+                <button 
+                  type='button'
+                  onClick={closeModal}
+                >
+                  <Image 
+                    src="/close.svg"
+                    alt='close'
+                    width={20}
+                    height={20}
+                    className='objct-contain'
+                  />
+                </button>
+              </Dialog.Panel>
+            </Transition.Child>
+        
+          </div>
+        </div>
+       </Dialog>
+      
+      </Transition>
+    </>
   )
 }
 
